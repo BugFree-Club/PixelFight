beedd2d9042c5659bf54ebc08ef734a0a651e5a2
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-07-23 22:27:38 +0800

    初始化PixelFight工程(UWP)与C#实验工程

---
+++ pfProject_CS/TrialProject/TrialProject/src/PfGrid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrialProject.src {
+    class PFGrid {
+        private String type;
+        private String attribution;
+        private int value;
+
+        public PFGrid() {
+
+        }
+
+        public PFGrid(String _t,String _a,int _v) {
+            Type = _t;
+            Attribution = _a;
+            Value = _v;
+        }
+
+        public string Type { get => type; set => type = value; }
+        public string Attribution { get => attribution; set => attribution = value; }
+        public int Value { get => value; set => this.value = value; }
+    }
+}
