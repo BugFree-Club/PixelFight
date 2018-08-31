540fa9333b5b3e63d617472c48ba3eb53954527a
Zhiquan.Wang <mySabreHawk@outlook.com>
2018-08-31 23:23:32 +0800

    Add Unity Project

---
+++ Pixel-Fight/Assets/Scripts/PfMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class PixelMap {
+    private String height;
+    private string width;
+    private string gridMap;
+    public PixelMap(String _h, String _w) {
+        Height = _h;
+        Width = _w;
+        GridMap = null;
+    }
+    public PixelMap() { }
+    public string Height { get => height; set => height = value; }
+    public string Width { get => width; set => width = value; }
+    public string GridMap { get => gridMap; set => gridMap = value; }
+}
---
+++ Pixel-Fight/Assets/Scripts/PfPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TrialProject.src{
+    public class PixelFightPlayer{
+        private string usrName;
+        private string loginId;
+        public PixelFightPlayer(String usrName, String loginId){
+            UsrName = usrName;
+            LoginId = loginId;
+        }
+        public PixelFightPlayer() { }
+        public string UsrName { get => usrName; set => usrName = value; }
+        public string LoginId { get => loginId; set => loginId = value; }
+    }
+}
