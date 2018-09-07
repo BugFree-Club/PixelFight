da749d142a2a125f1c6990fec42b5a15fc84b1bb
assassin945 <40627941+assassin945@users.noreply.github.com>
2018-09-07 23:45:55 +0800

    Add files via upload

--- pfProject_CS/TrialProject/TrialProject/src/PfPlayer.cs
+++ pfProject_CS/TrialProject/TrialProject/src/PfPlayer.cs
@@ -4,12 +4,17 @@ namespace TrialProject.src{
     public class PixelFightPlayer{
         private string usrName;
         private string loginId;
-        public PixelFightPlayer(String usrName, String loginId){
+        private string socketInfo;
+        public PixelFightPlayer(String usrName, String loginId, String socket){
             UsrName = usrName;
             LoginId = loginId;
+            SocketInfo = socket;
         }
         public PixelFightPlayer() { }
         public string UsrName { get => usrName; set => usrName = value; }
         public string LoginId { get => loginId; set => loginId = value; }
+        public string SocketInfo { get => socketInfo; set => socketInfo = value; }
+
+        
     }
 }
---
+++ pfProject_CS/TrialProject/TrialProject/src/PfSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace TrialProject.src{
+    public class PixelFightSystem
+    {
+        public String NetworkSev;
+        public String Game;
+        public PixelFightSystem() { }
+        public PixelFightSystem(String ip, String port)
+        {
+            NetworkSev = netWorkService(ip, port);
+            Game = pixelFightGame();
+        }
+        private string pixelFightGame()
+        {
+            throw new NotImplementedException();
+        }
+        private string netWorkService(string ip, string port)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
